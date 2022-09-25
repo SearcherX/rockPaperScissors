297f2aac12d5c399ab97380492037bb177cf907d
SearcherX <93086610+SearcherX@users.noreply.github.com>
2022-09-25 19:42:45 +0300

    Game v1.8

--- src/communication/UDPClient.java
+++ src/communication/UDPClient.java
@@ -9,49 +9,37 @@ public class UDPClient implements IUDPSocket {
     private int serverPort = 0;
 
     public UDPClient(String serverIP, int serverPort) throws UnknownHostException, SocketException {
-        //try {
-            this.serverIP = InetAddress.getByName(serverIP);
-            this.serverPort = serverPort;
-            clientSocket = new DatagramSocket();
-        //} catch (SocketException | UnknownHostException e) {
-            //e.printStackTrace();
-        //}
+        this.serverIP = InetAddress.getByName(serverIP);
+        this.serverPort = serverPort;
+        clientSocket = new DatagramSocket();
     }
 
     public void send(String msg) throws IOException {
-        //try {
-            /* Создайте буфер для хранения отправляемых данных. */
-            byte[] sendingDataBuffer = new byte[1024];
-            // Преобразуйте строку в байты и сохраните в буфер
-            sendingDataBuffer = msg.getBytes();
+        /* Создайте буфер для хранения отправляемых данных. */
+        byte[] sendingDataBuffer = new byte[1024];
+        // Преобразуйте строку в байты и сохраните в буфер
+        sendingDataBuffer = msg.getBytes();
 
-            // Создайте UDP-пакет
-            DatagramPacket sendingPacket = new DatagramPacket(sendingDataBuffer, sendingDataBuffer.length,
-                    serverIP, serverPort);
+        // Создайте UDP-пакет
+        DatagramPacket sendingPacket = new DatagramPacket(sendingDataBuffer, sendingDataBuffer.length,
+                serverIP, serverPort);
 
-            // Отправьте UDP-пакет серверу
-            clientSocket.send(sendingPacket);
-        //} catch (IOException e) {
-            //e.printStackTrace();
-        //}
+        // Отправьте UDP-пакет серверу
+        clientSocket.send(sendingPacket);
     }
 
     public String receive() throws IOException {
         String receivedData = null;
-        //try {
-            /* Создайте буфер для хранения получаемых данных. */
-            byte[] receivingDataBuffer = new byte[1024];
+        /* Создайте буфер для хранения получаемых данных. */
+        byte[] receivingDataBuffer = new byte[1024];
             /* Создайте экземпляр UDP-пакета для хранения клиентских данных с использованием буфера
             для полученных данных */
-            DatagramPacket receivingPacket = new DatagramPacket(receivingDataBuffer, receivingDataBuffer.length);
-            // Получите данные от клиента и сохраните их в receivingPacket
-            clientSocket.receive(receivingPacket);
+        DatagramPacket receivingPacket = new DatagramPacket(receivingDataBuffer, receivingDataBuffer.length);
+        // Получите данные от клиента и сохраните их в receivingPacket
+        clientSocket.receive(receivingPacket);
 
-            // Преобразуйте данные в строку
-            receivedData =  new String(receivingPacket.getData(), 0, receivingPacket.getLength());
-        //} catch (IOException e) {
-           // e.printStackTrace();
-        //}
+        // Преобразуйте данные в строку
+        receivedData = new String(receivingPacket.getData(), 0, receivingPacket.getLength());
         return receivedData;
     }
 
--- src/communication/UDPServer.java
+++ src/communication/UDPServer.java
@@ -6,18 +6,14 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
 
-public class UDPServer  implements IUDPSocket {
+public class UDPServer implements IUDPSocket {
     private DatagramSocket serverSocket = null;
     private InetAddress senderAddress;
     private int senderPort;
 
 
     public UDPServer(int port) throws SocketException {
-        //try {
-            serverSocket = new DatagramSocket(port);
-        //} catch (SocketException e) {
-            //e.printStackTrace();
-        //}
+        serverSocket = new DatagramSocket(port);
     }
 
     public InetAddress getSenderAddress() {
@@ -30,46 +26,38 @@ public class UDPServer  implements IUDPSocket {
 
     public String receive() throws IOException {
         String receivedData = null;
-        //try {
-             /* Создайте буфер для хранения получаемых данных. */
-            byte[] receivingDataBuffer = new byte[1024];
+        /* Создайте буфер для хранения получаемых данных. */
+        byte[] receivingDataBuffer = new byte[1024];
 
             /* Создайте экземпляр UDP-пакета для хранения клиентских данных с использованием буфера
             для полученных данных */
-            DatagramPacket inputPacket = new DatagramPacket(receivingDataBuffer, receivingDataBuffer.length);
+        DatagramPacket inputPacket = new DatagramPacket(receivingDataBuffer, receivingDataBuffer.length);
 
-            // Получите данные от клиента и сохраните их в inputPacket
-            serverSocket.receive(inputPacket);
-            // Преобразуйте данные в строку
-            receivedData = new String(inputPacket.getData(), 0, inputPacket.getLength());
+        // Получите данные от клиента и сохраните их в inputPacket
+        serverSocket.receive(inputPacket);
+        // Преобразуйте данные в строку
+        receivedData = new String(inputPacket.getData(), 0, inputPacket.getLength());
 
-            // Получите IP-адрес и порт клиента
-            senderAddress = inputPacket.getAddress();
-            senderPort = inputPacket.getPort();
-        //} catch (IOException e) {
-            //e.printStackTrace();
-        //}
+        // Получите IP-адрес и порт клиента
+        senderAddress = inputPacket.getAddress();
+        senderPort = inputPacket.getPort();
         return receivedData;
     }
 
     public void send(String msg) throws IOException {
-        //try {
-            /* Создайте буфер для хранения отправляемых данных. */
-            byte[] sendingDataBuffer;
-            // Преобразуйте строку в байты и сохраните в буфер
-            sendingDataBuffer = msg.getBytes();
+        /* Создайте буфер для хранения отправляемых данных. */
+        byte[] sendingDataBuffer;
+        // Преобразуйте строку в байты и сохраните в буфер
+        sendingDataBuffer = msg.getBytes();
 
-            // Создайте новый UDP-пакет с данными, чтобы отправить их клиенту
-            DatagramPacket outputPacket = new DatagramPacket(
-                    sendingDataBuffer, sendingDataBuffer.length,
-                    senderAddress, senderPort
-            );
+        // Создайте новый UDP-пакет с данными, чтобы отправить их клиенту
+        DatagramPacket outputPacket = new DatagramPacket(
+                sendingDataBuffer, sendingDataBuffer.length,
+                senderAddress, senderPort
+        );
 
-            // Отправьте пакет клиенту
-            serverSocket.send(outputPacket);
-        //} catch (IOException e) {
-            //e.printStackTrace();
-        //}
+        // Отправьте пакет клиенту
+        serverSocket.send(outputPacket);
     }
 
     public void close() {
